39fc6a3edd5c0ed413447c9ab630bc1c2b6f9a8f
Lukas Meitz <lukasmariusmeitz@gmail.com>
2021-02-24 13:05:33 +0100

    getting closer

--- tests/test_imaging.py
+++ tests/test_imaging.py
@@ -2,6 +2,6 @@ from modules.visuals.imaging import *
 
 def test_imaging():
 
-    image = load_image('data/gtm_data/images/img1.png')
+    image = load_image('/home/lukas/jetson-cuda/data/gtm_data/images/img1.png')
     draw_lines(image, [[0, 0, 300, 300]], color=(255, 0, 0))
-    save_image(image, 'results/image_out.png')
+    save_image(image, '/home/lukas/jetson-cuda/results/image_out.png')
